dabb072460e4169af21837471247aed106b81173
gisa chris <gisachris24@gmail.com>
2023-09-13 18:58:24 -0200

    implement public_recipes index view and Styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'public_recipes/index'
   root 'users#index'
   devise_for :users
 
@@ -7,6 +8,7 @@ Rails.application.routes.draw do
       resources :recipe_food, only: [:new, :create], as: 'recipejointfoods'
     end
     resources :foods, only: [:index, :new, :create, :destroy]
+    resources :public_recipes, only: [:index]
     resources :recipe_foods, only: [:index, :show]
     resources :shopping_list, only: :index
   end
