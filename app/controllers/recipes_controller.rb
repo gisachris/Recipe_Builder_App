33a446970361510d6fafee499fb92f417ae3a634
gisa chris <gisachris24@gmail.com>
2023-09-13 10:42:36 -0200

    Add Recipes Index Html File And Controller Action

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,6 @@
 class RecipesController < ApplicationController
+  def index
+    @user = User.find(params[:user_id])
+    @recipes = Recipe.includes(:user).where(user: @user).references(:user)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,8 @@ Rails.application.routes.draw do
   root 'users#index'
   devise_for :users
 
-  resources :users do
-  resources :recipes, except: :update
-  resources :foods, except: :update
-  resources :shopping_list, only: :index
-  end
+  resources :users
+  resources :recipes
+  resources :foods
+  resources :recipe_foods
 end
