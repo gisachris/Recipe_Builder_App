70654685cc0fdf45a78f4f74b1fc02e169569cd0
gisa chris <gisachris24@gmail.com>
2023-09-13 14:47:08 -0200

    implement Recipes Form And Form Styling

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,4 +3,36 @@ class RecipesController < ApplicationController
     @user = User.find(params[:user_id])
     @recipes = Recipe.includes(:user).where(user: @user).references(:user)
   end
+
+  def show
+  end
+
+  def new
+    @user = current_user
+    @recipe = @user.recipes.build
+  end
+
+  def create
+    @user = current_user
+    @recipe = @user.recipes.build(recipe_params)
+
+    if @recipe.save
+      redirect_to user_recipes_path(@user), notice: 'Created the recipe successfully'
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @recipe = Recipe.find(params[:id])
+    if @recipe.destroy
+      redirect_to user_recipes_path(current_user)
+    end
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:name,:preparation_time,:cooking_time,:description)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :users do
-    resources :recipes, except: :update do
+    resources :recipes, only: [:index, :new, :show, :create, :destroy] do
       resources :recipe_food, only: [:new, :create], as: 'recipejointfoods'
     end
     resources :foods, only: [:index, :new, :create, :destroy]
